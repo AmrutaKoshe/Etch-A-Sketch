12f767637eb0a0b84f70b0fd8d8a030dcf2ad981
Amruta Koshe <amruta.koshe@gmail.com>
2021-02-27 12:04:05 +0530

    created grid layout

--- sketch.js
+++ sketch.js
@@ -1,8 +1,16 @@
 var contain = document.getElementById("container");
 
-for(var i=0; i<16; i++){
+//variable to denote number of rows or columns in the grid
+var n = 16;
+
+//create n*n divs 
+for(var i=0; i<n*n; i++){
     var grid = document.createElement("div");
-    grid.innerHTML = "hey";
-    grid.style.color = "red";
-    document.contain.appendChild(grid);
+    grid.style.paddingTop = "90%";
+    grid.style.backgroundColor = "white";
+    grid.style.border ="black 1px solid";
+    contain.appendChild(grid);
 }
+
+//create n columns in the grid.
+contain.style.gridTemplateColumns = `repeat(${n},auto)`;
