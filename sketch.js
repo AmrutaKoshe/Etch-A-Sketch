144b6b944985cb112d6af6dbc39324698c6fe92d
Amruta Koshe <amruta.koshe@gmail.com>
2021-03-02 13:25:14 +0530

    added slider

--- sketch.js
+++ sketch.js
@@ -1,40 +1,62 @@
 var contain = document.getElementById("container");
+var n = 20;
 
-//variable to denote number of rows or columns in the grid
-var n = 26;
+// for(let i=0; i<n*n; i++){
+//     var grid = document.createElement("div");
+//     grid.className = "element";
 
-//create n*n divs 
-for(let i=0; i<n*n; i++){
-    var grid = document.createElement("div");
-    grid.className = "element";
+//     //size of the grid 
+//     var size = 94 - Math.ceil(n/1.9);
+//     grid.style.paddingTop = `${size}%`;
 
-    //size of the grid 
-    var size = 94 - Math.ceil(n/1.9);
-    grid.style.paddingTop = `${size}%`;
+//     grid.style.border ="#b3b3b3 1px solid";
+//     contain.appendChild(grid);
+// }
 
-    grid.style.border ="black 1px solid";
-    contain.appendChild(grid);
-}
+document.getElementById("myRange").addEventListener("change", function(){
 
-//create n columns in the grid.
-contain.style.gridTemplateColumns = `repeat(${n},auto)`;
+    //variable to denote number of rows or columns in the grid
+    n = document.getElementById("myRange").value;
 
-//storing all the grids in an array class
-var gridChange = document.getElementsByClassName("element");
+    //remove all elements from the class
+    var rem = document.getElementsByClassName('element');
+    while(rem[0]) {
+        rem[0].parentNode.removeChild(rem[0]);
+    }
 
-//iterating through the array for mouseover event and 
-//changing the colour of each when the mouse hovers over it.
-for (let i = 0; i < gridChange.length; i++){
-    gridChange[i].addEventListener("mouseover", function(){
-        this.classList.add("hover");
-    });
-}
+    //replace the class with new n*n divs 
+    for(let i=0; i<n*n; i++){
+        var grid = document.createElement("div");
+        grid.className = "element";
 
-document.getElementById("reset").addEventListener("click",function(){
-    for (let i = 0; i < gridChange.length; i++)
-        gridChange[i].classList.remove("hover");
+        //size of the grid 
+        var size = 94 - Math.ceil(n/1.9);
+        grid.style.paddingTop = `${size}%`;
+
+        grid.style.border ="#b3b3b3 1px solid";
+        contain.appendChild(grid);
+    }
+
+    //create n columns in the grid.
+    contain.style.gridTemplateColumns = `repeat(${n},auto)`;
+
+    //storing all the grids in an array class
+    var gridChange = document.getElementsByClassName("element");
+
+    //iterating through the array for mouseover event and 
+    //changing the colour of each when the mouse hovers over it.
+    for (let i = 0; i < gridChange.length; i++){
+        gridChange[i].addEventListener("mouseover", function(){
+            this.classList.add("hover");
+        });
+    }
+
+    //reset the grid
+    document.getElementById("reset").addEventListener("click",function(){
+        for (let i = 0; i < gridChange.length; i++)
+            gridChange[i].classList.remove("hover");
+    })
+
 })
-
-
 
 
